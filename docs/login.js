c2f26db68d786df9132fa470d366ac96b4583060
Emmet Blassingame <100320217+em-blass@users.noreply.github.com>
2023-11-15 19:50:27 -0700

    Update login.js

--- docs/login.js
+++ docs/login.js
@@ -4,7 +4,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if (username === 'demoUser' && password === 'demoPass') {
+    if (username === 'admin' && password === 'admin') {
         alert('Login successful!');
         // Redirect to another page or perform some action
         window.location.href = 'welcome.html'; // Redirect to a welcome page
