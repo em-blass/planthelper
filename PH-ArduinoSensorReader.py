a7349a6b1379db1ce1721496244eca87fe5b42c5
Emmet Blassingame <100320217+em-blass@users.noreply.github.com>
2023-12-12 11:56:47 -0700

    Update PH-ArduinoSensorReader.py

--- PH-ArduinoSensorReader.py
+++ PH-ArduinoSensorReader.py
@@ -1,7 +1,9 @@
 import boto3
+import serial
 from time import sleep, time
 from datetime import datetime
-import serial
+from random import randint
+
 
 # AWS SETUP
 region = "us-east-2"
@@ -11,13 +13,11 @@ dynamodb = boto3.resource('dynamodb', aws_access_key_id=accessKeyId, aws_secret_
 
 # Initialize serial connection to Arduino
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-arduino.flush()
 
-# DICTIONARIES
 variables = {}
 
-# MISC
 lastWaterTime = time()
+
 
 def defaultVariables():
     dict = {}
@@ -40,7 +40,7 @@ def readVariables():
     try:
         for label in ["dirtmoisture", "light", "temp", "humidity", "global"]:
             response = tableVariables.get_item(Key={"label": label})
-            if "Item" in response:
+            if ("Item" in response):
                 dict[label] = response["Item"]
         
         tableVariables.update_item(
@@ -55,12 +55,12 @@ def readVariables():
         return variables
     return dict
     
-def writeData(dirtmoisture=0, light=0, temp=0, humidity=0, watered=False):
+def writeData(dirtmoisture = 0, light = 0, temp = 0, humidity = 0, watered = False):
     tableData = dynamodb.Table("Data")
     response = tableData.get_item(Key={"id": -1})
     newID = response["Item"]["max"] + 1
     timeString = datetime.now().strftime("%Y-%m-%d %H:%M")
-    tableData.put_item(Item={
+    tableData.put_item(Item = {
         "id": newID,
         "time": timeString,
         "dirtmoisture": dirtmoisture,
@@ -70,25 +70,31 @@ def writeData(dirtmoisture=0, light=0, temp=0, humidity=0, watered=False):
         "watered": watered
     })
     
-    tableData.put_item(Item={
+    tableData.put_item(Item = {
         "id": -1,
         "max": newID
     })
-
+    
 def readSensors():
     if arduino.in_waiting > 0:
         line = arduino.readline().decode('utf-8').rstrip()
-        soilMoisture, light, temperature, humidity = map(int, line.split(","))
-        return {
-            "dirtmoisture": soilMoisture,
-            "light": light,
-            "temp": temperature,
-            "humidity": humidity
-        }
+        # Assuming Arduino sends data in 'value1,value2,value3,value4' format
+        values = line.split(',')
+        if len(values) == 4:
+            return {
+                "dirtmoisture": int(values[0]),
+                "light": int(values[1]),
+                "temp": int(values[2]),
+                "humidity": int(values[3])
+            }
     return None
 
 def water():
-    arduino.write(b'WATER\n')
+    try:
+        arduino.write("WATER\n".encode())  # Sending the command to the Arduino
+        print("Sent water command to Arduino")
+    except Exception as e:
+        print("Error sending water command:", e)
 
 def waterAlgorithm(data):
     global lastWaterTime
@@ -105,24 +111,28 @@ def waterAlgorithm(data):
     weight, max, current = algorithmHelper("humidity", data)
     humidityPercent = weight * (1 - ((max - current) / max))
     
-    totalPercent = dirtPercent # + other percents
+    totalPercent = dirtPercent + lightPercent + tempPercent + humidityPercent
     maxTime = int(variables["global"]["maxtime"])
     minTime = int(variables["global"]["mintime"])
     dirtCurrent = float(data["dirtmoisture"])
     emergencyminval = float(variables["dirtmoisture"]["emergencyminval"])
-    delay = (maxTime - minTime) * (1 - totalPercent) + minTime
+    delay = (maxTime - minTime) * (1-totalPercent) + minTime
     doStandardWater = (lastWaterTime + delay) <= time()
     doEmergencyWater = ((lastWaterTime + minTime) > time()) and (dirtCurrent <= emergencyminval)
     doForceWater = variables["global"]["forcewater"]
-    
-    if doStandardWater or doEmergencyWater or doForceWater:
+    print("last:", lastWaterTime, "delay:", delay, "t+delay:", lastWaterTime+delay, "time", time())
+    if (doStandardWater or doEmergencyWater or doForceWater):
         lastWaterTime = time()
         return True
     else:
         return False
     
+    
+    
 def algorithmHelper(label, data):
     return float(variables[label]["weight"]), float(variables[label]["maxval"]), float(data[label])
+
+    
     
 def loop():
     global variables
@@ -130,12 +140,17 @@ def loop():
         variables = readVariables()
         sleep(int(variables["global"]["intervaltime"]))
         sensorReadings = readSensors()
-        if sensorReadings is not None:
-            sensorReadings["watered"] = waterAlgorithm(sensorReadings)
-            if sensorReadings["watered"]:
-                water()
-            writeData(**sensorReadings)
+        sensorReadings["watered"] = waterAlgorithm(sensorReadings)
+        if (sensorReadings["watered"]):
+            water()
+        writeData(dirtmoisture = sensorReadings["dirtmoisture"],
+                  light = sensorReadings["light"],
+                  temp = sensorReadings["temp"],
+                  humidity = sensorReadings["humidity"],
+                  watered = sensorReadings["watered"])
 
 if __name__ == "__main__":
     variables = defaultVariables()
     loop()
+    
+    
