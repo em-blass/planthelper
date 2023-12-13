2473f12d437d0f01c7aa125b55e66bb0a0625d2b
Emmet Blassingame <100320217+em-blass@users.noreply.github.com>
2023-12-12 18:20:36 -0700

    Update PH-ArduinoSensorReader.py

--- PH-ArduinoSensorReader.py
+++ PH-ArduinoSensorReader.py
@@ -12,10 +12,8 @@ accessKeyId = "AKIA5HSVJ3PMIMSZCQ76"
 secretAccessKey = "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"
 dynamodb = boto3.resource('dynamodb', aws_access_key_id=accessKeyId, aws_secret_access_key=secretAccessKey, region_name=region)
 
-# Initialize serial connection to Arduino
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-# Constants for dirt moisture range
 MIN_DIRT_MOISTURE = 520
 MAX_DIRT_MOISTURE = 840
 
@@ -51,12 +49,16 @@ def readSensors():
         line = arduino.readline().decode('utf-8').rstrip()
         values = line.split(',')
         if len(values) == 4:
-            return {
-                "dirtmoisture": round((int(values[1]) - MIN_DIRT_MOISTURE) / (MAX_DIRT_MOISTURE - MIN_DIRT_MOISTURE) * 100),
-                "light": int(values[0]),
-                "temp": int(float(values[2])),
-                "humidity": round(float(values[3]))
-            }
+            try:
+                return {
+                    "dirtmoisture": round((int(values[1]) - MIN_DIRT_MOISTURE) / (MAX_DIRT_MOISTURE - MIN_DIRT_MOISTURE) * 100),
+                    "light": int(values[0]),
+                    "temp": int(float(values[2])),
+                    "humidity": round(float(values[3]))
+                }
+            except ValueError as e:
+                print(f"ValueError in readSensors: {e}")
+                return None
     return None
 
 def forceWaterCommand():
@@ -69,6 +71,35 @@ def check_for_command(cmd_queue):
     while True:
         cmd = input()
         cmd_queue.put(cmd)
+        
+def checkForceWater():
+    try:
+        table = dynamodb.Table("Variables") 
+        response = table.get_item(Key={"label": "global"})
+        if 'Item' in response and response['Item']['forcewater']:
+            return response['Item']['forcewater']
+    except botocore.exceptions.ClientError as e:
+        print(f"ClientError in checkForceWater: {e}")
+    except botocore.exceptions.BotoCoreError as e:
+        print(f"BotoCoreError in checkForceWater: {e}")
+    except Exception as e:
+        print(f"ERROR in checkForceWater: {e}")
+    return False
+
+def resetForceWater():
+    try:
+        table = dynamodb.Table("Variables")
+        table.update_item(
+            Key={"label": "global"},
+            UpdateExpression="set forcewater = :val",
+            ExpressionAttributeValues={":val": False}
+        )
+    except botocore.exceptions.ClientError as e:
+        print(f"ClientError in resetForceWater: {e}")
+    except botocore.exceptions.BotoCoreError as e:
+        print(f"BotoCoreError in resetForceWater: {e}")
+    except Exception as e:
+        print(f"ERROR in resetForceWater: {e}")
 
 def loop():
     cmd_queue = queue.Queue()
@@ -76,6 +107,9 @@ def loop():
     cmd_thread.start()
 
     while True:
+        if checkForceWater():
+            forceWaterCommand()
+
         if not cmd_queue.empty():
             cmd = cmd_queue.get()
             if cmd == "FORCE_WATER":
@@ -90,7 +124,11 @@ def loop():
             print(f"Humidity: {sensorReadings['humidity']}%")
             writeData(sensorReadings['dirtmoisture'], sensorReadings['light'], sensorReadings['temp'], sensorReadings['humidity'])
 
-        sleep(2)  # Delay between readings
+        if checkForceWater():
+            forceWaterCommand()
+            resetForceWater()
+
+        sleep(2)
 
 if __name__ == "__main__":
     loop()
